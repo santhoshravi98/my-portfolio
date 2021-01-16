64e307baaeb7a166c38279b0c16c5396723ba99d
Santhosh <rsanthosh411998@gmail.com>
2021-01-16 23:14:05 +0530

    Changes for Content in timeline and footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,8 +5,7 @@ import {
   faTwitter,
   faLinkedin,
   faInstagram,
-  faGithub,
-  faWhatsapp,
+  faGithub
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -17,46 +16,30 @@ const Footer = (props) => {
         <div class="container">
           <div class="row">
             <div class="col-sm-12 col-md-6">
-              <h6>About</h6>
+              <h6>About this Site</h6>
               <p class="text-justify">
-                Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative
-                to help the upcoming programmers with the code. Scanfcode
-                focuses on providing the most efficient code or snippets as the
-                code wants to be simple. We will help programmers build up
-                concepts in different programming languages that include C, C++,
-                Java, HTML, CSS, Bootstrap, JavaScript, PHP, Android, SQL and
-                Algorithm.
+               This site is assembled from different web sources. Tools used include React, Redux, CSS. Have used several plugins to support several features in React. These include CSS Modules, React-Bootstrap, Css-module-class-lister, fortawesome/free-brands-svg-icons,..etc
+               Firebase is used as a source of Data Storage. I am happy to share the list of all site references that I used. (Click here). Ping me if you have any questions or Feedback. 
               </p>
             </div>
 
             <div class="col-xs-6 col-md-3">
-              <h6>Categories</h6>
+              <h6>Future Plans</h6>
               <ul className={css.footerlinks}>
                 <li>
-                  <a href="http://scanfcode.com/category/c-language/">C</a>
+                  Chat Bot
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/category/front-end-development/">
-                    UI Design
-                  </a>
+                  Live Notifications
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/category/back-end-development/">
-                    PHP
-                  </a>
+                  Automated Mail Response
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/category/java-programming-language/">
-                    Java
-                  </a>
+                 Analytics Charts
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/category/android/">Android</a>
-                </li>
-                <li>
-                  <a href="http://scanfcode.com/category/templates/">
-                    Templates
-                  </a>
+                  Interactive Responsive Design
                 </li>
               </ul>
             </div>
@@ -65,23 +48,23 @@ const Footer = (props) => {
               <h6>Quick Links</h6>
               <ul className={css.footerlinks}>
                 <li>
-                  <a href="http://scanfcode.com/about/">About Us</a>
+                  <a href="/work-experience">Work</a>
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/contact/">Contact Us</a>
+                  <a href="/stories">Travel Stories</a>
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/contribute-at-scanfcode/">
-                    Contribute
+                  <a href="/resume">
+                    Resume
                   </a>
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/privacy-policy/">
-                    Privacy Policy
+                  <a href="/feedback">
+                    Feedback
                   </a>
                 </li>
                 <li>
-                  <a href="http://scanfcode.com/sitemap/">Sitemap</a>
+                  <a href="/">Home</a>
                 </li>
               </ul>
             </div>
@@ -92,41 +75,36 @@ const Footer = (props) => {
           <div class="row">
             <div class="col-md-8 col-sm-6 col-xs-12">
               <p class={css.copyrighttext}>
-                Copyright &copy; 2017 All Rights Reserved by
-                <a href="#">Scanfcode</a>.
+                Copyright &copy; 2021 No Rights Reserved.
+                <a href="#">Thanks</a>.
               </p>
             </div>
 
             <div class="col-md-4 col-sm-6 col-xs-12">
               <ul className={css.socialicons}>
                 <li>
-                  <a className="facebook" href="#">
+                  <a className="facebook" href="https://www.facebook.com/rsanthosh411998/" rel="noopener noreferrer" target="_blank">
                     {" "}
                     <FontAwesomeIcon icon={faFacebook} />
                   </a>
                 </li>
                 <li>
-                  <a className="twitter" href="#">
+                  <a className="twitter" href="#" rel="noopener noreferrer" target="_blank">
                     <FontAwesomeIcon icon={faTwitter} />
                   </a>
                 </li>
                 <li>
-                  <a className="dribbble" href="#">
+                  <a className="dribbble" href="https://www.instagram.com/invites/contact/?i=1xan13ua2xdjb&utm_content=yy6lc5" rel="noopener noreferrer"  target="_blank">
                     <FontAwesomeIcon icon={faInstagram} />
                   </a>
                 </li>
                 <li>
-                  <a className="linkedin" href="#">
+                  <a className="linkedin" href="https://www.linkedin.com/in/santhosh-ravi-380565137/" rel="noopener noreferrer" target="_blank">
                     <FontAwesomeIcon icon={faLinkedin} />
                   </a>
                 </li>
                 <li>
-                  <a className="linkedin" href="#">
-                    <FontAwesomeIcon icon={faWhatsapp} />
-                  </a>
-                </li>
-                <li>
-                  <a className="linkedin" href="#">
+                  <a className="linkedin" href="https://github.com/santhoshravi98/" rel="noopener noreferrer" target="_blank">
                     <FontAwesomeIcon icon={faGithub} />
                   </a>
                 </li>
--- src/Components/HomePageTimeLineDesktop/HomePageTimeLineDesktop.js
+++ src/Components/HomePageTimeLineDesktop/HomePageTimeLineDesktop.js
@@ -1,107 +1,95 @@
 import Auxillary from "../../HOC/Auxillary/Auxillary";
 import css from "../HomePageTimeLineDesktop/HomePageTimeLineDesktop.module.css";
+import stanesSchoolPhoto from '../../Assets/Images/Stanes_School_Coonoor.jpg'
+import stanesSchoolPhoto1 from '../../Assets/Images/Stanes_School_Coonoor1.png'
+import cegPhoto from '../../Assets/Images/Annadurai_statue.jpg'
+import xomePhoto from '../../Assets/Images/xome.jpg'
 const HomePageTimeLineDesktop = (props) => {
-  return (
-    <Auxillary>
-      <section className={css.timeline}>
-        <div className={css.tlitem}>
-          <div
-            className={css.tlbg}
-            style={{
-              backgroundImage: 'url(static/media/profile_photo.jpg)',
-            }}
-          ></div>
+    return (
+        <Auxillary>
+            <section className={css.timeline}>
+                <div className={css.tlitem}>
+                    <div
+                        className={css.tlbg}
+                        style={{
+                            backgroundImage: `url(${stanesSchoolPhoto})`,
+                        }}
+                    ></div>
 
-          <div className={css.tlyear}>
-            <p>2011</p>
-          </div>
+                    <div className={css.tlyear}>
+                        <p>2013</p>
+                    </div>
 
-          <div className={css.tlcontent}>
-            <h1>Lorem ipsum dolor sit</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit.
+                    <div className={css.tlcontent}>
+                        <h1>Stanes School Coonoor</h1>
+                        <p>
+                            Located in the hill top of Coonoor. Joined Stanes in 7'th Std and completed my High School. It's a Beautiful School with Beautiful Memories.
             </p>
-          </div>
-        </div>
+                    </div>
+                </div>
 
-        <div className={css.tlitem}>
-          <div
-            className={css.tlbg}
-            style={{
-              backgroundImage: "url(https://placeimg.com/802/802/nature)",
-            }}
-          ></div>
+                <div className={css.tlitem}>
+                    <div
+                        className={css.tlbg}
+                        style={{
+                            backgroundImage: `url(${stanesSchoolPhoto1})`,
+                        }}
+                    ></div>
 
-          <div className={css.tlyear}>
-            <p className="f2 heading--sanSerif">2013</p>
-          </div>
+                    <div className={css.tlyear}>
+                        <p className="f2 heading--sanSerif">2015</p>
+                    </div>
 
-          <div className={css.tlcontent}>
-            <h1 className="f3 text--accent ttu">Vestibulum laoreet lorem</h1>
-            <p>
-              Suspendisse potenti. Sed sollicitudin eros lorem, eget accumsan
-              risus dictum id. Maecenas dignissim ipsum vel mi rutrum egestas.
-              Donec mauris nibh, facilisis ut hendrerit vel, fringilla sed
-              felis. Morbi sed nisl et arcu.
+                    <div className={css.tlcontent}>
+                        <h1 className="f3 text--accent ttu">Stanes School Coonoor</h1>
+                        <p>
+                           Completed my Higher Secondary too in my Favourite School. Each and Every Place holds Million Memories.
             </p>
-          </div>
-        </div>
+                    </div>
+                </div>
 
-        <div className={css.tlitem}>
-          <div
-            className={css.tlbg}
-            style={{
-              backgroundImage: "url(https://placeimg.com/801/801/nature)",
-            }}
-          ></div>
+                <div className={css.tlitem}>
+                    <div
+                        className={css.tlbg}
+                        style={{
+                            backgroundImage: `url(${cegPhoto})`,
+                        }}
+                    ></div>
 
-          <div className={css.tlyear}>
-            <p className="f2 heading--sanSerif">2014</p>
-          </div>
+                    <div className={css.tlyear}>
+                        <p className="f2 heading--sanSerif">2019</p>
+                    </div>
 
-          <div className={css.tlcontent}>
-            <h1 className="f3 text--accent ttu">Phasellus mauris elit</h1>
-            <p>
-              Mauris cursus magna at libero lobortis tempor. Suspendisse
-              potenti. Aliquam interdum vulputate neque sit amet varius.
-              Maecenas ac pulvinar nisi. Fusce vitae nunc ultrices, tristique
-              dolor at, porttitor dolor. Etiam id cursus arcu, in dapibus nibh.
-              Pellentesque non porta leo. Nulla eros odio, egestas quis
-              efficitur vel, pretium sed.
+                    <div className={css.tlcontent}>
+                        <h1 className="f3 text--accent ttu">College Of Engineering Guindy</h1>
+                        <p>
+                            Completed my Bachelors in Computer Science and Engineering in CEG. Learnt to Live Life, not Engineering. Words can't describe my beautiful college life. 
+                            CEG Memories will remain forever.
             </p>
-          </div>
-        </div>
+                    </div>
+                </div>
 
-        <div className={css.tlitem}>
-          <div
-            className={css.tlbg}
-            style={{
-              backgroundImage: "url(https://placeimg.com/801/801/nature)",
-            }}
-          ></div>
+                <div className={css.tlitem}>
+                    <div
+                        className={css.tlbg}
+                        style={{
+                            backgroundImage: `url(${xomePhoto})`,
+                        }}
+                    ></div>
 
-          <div className={css.tlyear}>
-            <p className="f2 heading--sanSerif">2016</p>
-          </div>
+                    <div className={css.tlyear}>
+                        <p className="f2 heading--sanSerif">PRESENT</p>
+                    </div>
 
-          <div className={css.tlcontent}>
-            <h1 className="f3 text--accent ttu">Mauris vitae nunc elem</h1>
-            <p>
-              Suspendisse ac mi at dolor sodales faucibus. Nunc sagittis ornare
-              purus non euismod. Donec vestibulum efficitur tortor, eget
-              efficitur enim facilisis consequat. Vivamus laoreet laoreet elit.
-              Ut ut varius metus, bibendum imperdiet ex. Curabitur diam quam,
-              blandit at risus nec, pulvinar porttitor lorem. Pellentesque dolor
-              elit.
+                    <div className={css.tlcontent}>
+                        <h1 className="f3 text--accent ttu">Xome</h1>
+                        <p>
+                           Software Developer in Xome India. Working in Online Auctions Engine for Properties in US. I like what I am doing and interested in learning new things
             </p>
-          </div>
-        </div>
-      </section>
-    </Auxillary>
-  );
+                    </div>
+                </div>
+            </section>
+        </Auxillary>
+    );
 };
 export default HomePageTimeLineDesktop;
